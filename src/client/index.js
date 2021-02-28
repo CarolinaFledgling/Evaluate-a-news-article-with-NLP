a4fa313373ff944afb4cca010cd2f378ec247d4b
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-02-28 21:10:02 +0100

    install webpack

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import {
+    checkForName
+} from './js/nameChecker';
+import {
+    handleSubmit
+} from './js/formHandler';
+
+import './styles/modules/base.scss';
+import './styles/modules/footer.scss';
+import './styles/modules/form.scss';
+import './styles/modules/header.scss';
+import './styles/modules/reset.scss';
