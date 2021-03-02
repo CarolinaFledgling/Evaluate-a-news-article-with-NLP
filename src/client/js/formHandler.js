e719d3f53cc663b759e1c4eb3180e85855664c50
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-03-02 21:41:33 +0100

    fetched data from API and display on website

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,6 +5,10 @@ import {
 const inputUrl = document.querySelector('.form__url-input');
 const btnSubmit = document.querySelector('.btn-submit')
 const btnReset = document.querySelector('.btn-reset')
+const confidence = document.querySelector('.result__item--confidence')
+const agreement = document.querySelector('.result__item--agreement')
+const irony = document.querySelector('.result__item--irony')
+const subjectivity = document.querySelector('.result__item--subjectivity')
 const url = "https://api.meaningcloud.com/sentiment-2.1?key=";
 
 //`${url}${API_KEY}&lang=auto&url=""`
@@ -18,20 +22,24 @@ function handleSubmit(event) {
     const urlEnter = inputUrl.value;
 
     // receive api key from server side 
-
     fetch('/api_data')
         .then((res) => res.json())
         .then((config) => {
             console.log('APi aa', config)
+            //recive data from API 
             fetch(`${url}${config.key}&lang=auto&url=${urlEnter}`) // jak tutaj sprawdzić url ?
                 .then((res) => res.json())
                 .then((res) => {
                     console.log(res)
+                    confidence.innerHTML = ` Confidence ${res.confidence}`;
+                    agreement.innerHTML = ` Agreement ${res.agreement}`;
+                    irony.innerHTML = ` Irony ${res.irony}`;
+                    subjectivity.innerHTML = ` Subjectivity ${res.subjectivity}`;
                 }).catch((err) => {
                     console.log(err, 'something went wrong')
                 })
         })
-
+    inputUrl.value = "";
 
 
 
